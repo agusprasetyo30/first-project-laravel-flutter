7a75d928168c9011be8d44590b13463510eb0b2e
Agus Prasetyo <agusprasetyo1889@gmail.com>
2023-03-05 22:53:42 +0700

    finish session 1

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -10,3 +10,61 @@
  */
 
 
+namespace App\Models{
+/**
+ * App\Models\Category
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category query()
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Menu
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Menu newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Menu newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Menu query()
+ */
+	class Menu extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Place
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Place newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Place newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Place query()
+ */
+	class Place extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\SubDistrict
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|SubDistrict newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|SubDistrict newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|SubDistrict query()
+ */
+	class SubDistrict extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ */
+	class User extends \Eloquent {}
+}
+
--- database/migrations/2023_03_05_154104_create_categories_table.php
+++ database/migrations/2023_03_05_154104_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubDistrictsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSubDistrictsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sub_districts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
@@ -28,6 +28,6 @@ class CreateSubDistrictsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sub_districts');
+        Schema::dropIfExists('categories');
     }
 }
